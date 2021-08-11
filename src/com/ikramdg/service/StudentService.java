0fd169d6fb3644a2f272325d4ea4da297d3a66ea
İkram <ikramdgc@gmail.com>
2021-08-11 16:58:49 +0300

    student service

--- src/com/ikramdg/service/StudentService.java
+++ src/com/ikramdg/service/StudentService.java
@@ -7,80 +7,116 @@ import com.ikramdg.repository.StudentRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
 public class StudentService implements StudentRepository {
 
-    private EntityManager entityManager = JPAUtils.getEntityManagerFactory().createEntityManager();
+    private final EntityManager entityManager = JPAUtils.getEntityManagerFactory().createEntityManager();
 
 
     @Override
     public Address findAddressById(Long id) {
-        Address address = null;
-        EntityTransaction transaction = null;
-
-        try {
-            transaction = entityManager.getTransaction();
-
-            transaction.begin();
-            Student foundStudent = entityManager.find(Student.class, id);
-            if (foundStudent == null) {
-                throw new StaffNotFoundException(id);
-            }
-            address = foundStudent.getAddress();
-        }catch (StaffNotFoundException e){
-                transaction.rollback();
-                throw e;
-        }
-        transaction.commit();
-        return address;
-
+        return findById(id).getAddress();
     }
 
     @Override
     public List<Student> findAll() {
-        return null;
+       return  entityManager.createQuery("select s from Student s",Student.class).getResultList();
     }
 
     @Override
     public Student findById(Long id) {
-        return null;
+        EntityTransaction transaction = null;
+        Student foundStudent = null;
+        try {
+            transaction = entityManager.getTransaction();
+
+            transaction.begin();
+            foundStudent = entityManager.find(Student.class, id);
+            if (foundStudent == null) {
+                throw new StaffNotFoundException(id);
+            }
+        }catch (RuntimeException e) {
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+        transaction.commit();
+        return foundStudent;
     }
 
     @Override
-    public void saveToDatabase(Student object) {
-
+    public void saveToDatabase(Student student) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.persist(student);
+            transaction.commit();
+        }catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void deleteFromDatabase(Student object) {
-
+    public void deleteFromDatabase(Student student) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.remove(student);
+            transaction.commit();
+        }catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void deleteFromDatabase(Long id) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            Query query = entityManager.createQuery("delete from Student s where s.id = :id",Student.class).setParameter("id", id);
+            query.executeUpdate();
+            transaction.commit();
+        }catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
 
     }
 
     @Override
-    public void updateOnDatabase(Student object, Long id) {
+    public void updateOnDatabase(Student student, Long id) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            Student foundStudent = entityManager.find(Student.class, id);
+            foundStudent = student;
+            transaction.commit();
 
+        }catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
     }
 
     @Override
     public LocalDate findBirthDateById(Long id) {
-        return null;
+        return  findById(id).getBirthDate();
     }
 
     @Override
     public Gender findGenderById(Long id) {
-        return null;
+        return findById(id).getGender();
     }
 
     @Override
     public Set<Course> findCourses(Long id) {
-        return null;
+        return findById(id).getCourses();
     }
 }
